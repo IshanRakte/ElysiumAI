4b400559e59c4b8bc56a33f94cabe3ea8ea27aaf
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2023-12-17 21:00:35 +0530

    implemented google calendar integration

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ import webbrowser
 import openai
 import asyncio
 from datetime import datetime, timedelta
-import asyncio
 from telegram import Bot
 from telegram.helpers import escape_markdown
+from google_auth_oauthlib.flow import InstalledAppFlow
+import googleapiclient.discovery
+from dateutil import parser
+import threading
 
 api_key = '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'
 telegram_token = '6723957326:AAFby6ENH9QYHGhdjOvKGBk3PC0eMiQMjDY'
 telegram_chat_id = '-4077193516'
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+
+def get_google_calendar_service():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'client_secret_546479796583-hqkn2k7t4e08hiq0johqt284s794oq97.apps.googleusercontent.com.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=creds)
+    return service
 
 
 def generate_meeting_invite(agenda, time, duration, venue, name, date):
@@ -25,7 +38,39 @@ def generate_meeting_invite(agenda, time, duration, venue, name, date):
 
     assistant_response = response['choices'][0]['text']
 
-    return assistant_response
+    # Parse user-provided date and time
+    parsed_date = parser.parse(date)
+    parsed_time = parser.parse(time).time()
+
+    combined_datetime = datetime.combine(parsed_date, parsed_time)
+
+    duration_timedelta = timedelta(hours=float(duration))
+
+    end_datetime = combined_datetime + duration_timedelta
+
+    # Create Google Calendar event
+    try:
+        service = get_google_calendar_service()
+        calendar_id = '91b974b8b947d533c5523a1fae1167a3995e9121ae42b8d18877a439014729e5@group.calendar.google.com'
+        event = {
+            'summary': f'Meeting: {agenda}',
+            'location': venue,
+            'description': f'Agenda: {agenda}\nOrganizer: {name}',
+            'start': {
+                'dateTime': combined_datetime.strftime('%Y-%m-%dT%H:%M:%S'),
+                'timeZone': 'Asia/Kolkata',
+            },
+            'end': {
+                'dateTime': end_datetime.strftime('%Y-%m-%dT%H:%M:%S'),
+                'timeZone': 'Asia/Kolkata',
+            },
+        }
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
+    except Exception as e:
+        print(f"Error creating event: {e}")
+
+    return assistant_response, combined_datetime
 
 
 async def send_telegram_message(message, token, chat_id):
@@ -85,7 +130,7 @@ def cancel_meeting(agenda, start_datetime):
 
 
 if __name__ == '__main__':
-    print('Welcome to Jarvis AI')
+    print('Welcome to Elysium AI')
     speak("Elysium AI")
     while True:
         print("Listening...")
@@ -112,18 +157,20 @@ if __name__ == '__main__':
         elif "generate meeting invite" in query.lower():
             speak("Sure, let's create a meeting invite.")
 
+            name = "Ishan"
+            duration = "1"
             speak("What's the meeting date.")
             date = takecommand()
             speak("What's the meeting agenda.")
             agenda = takecommand()
             speak("What's the meeting time.")
             time = takecommand()
-            speak("What's the meeting duration.")
-            duration = takecommand()
+            # speak("What's the meeting duration.")
+            # duration = takecommand()
             speak("What's the meeting venue.")
             venue = takecommand()
 
-            meeting_invite = generate_meeting_invite(agenda, time, duration, venue, name, date)
+            meeting_invite, start_datetime = generate_meeting_invite(agenda, time, duration, venue, name, date)
             print(meeting_invite)
             schedule_reminders(start_datetime, agenda)
             speak(meeting_invite)
@@ -131,4 +178,4 @@ if __name__ == '__main__':
             asyncio.run(send_telegram_message(meeting_invite, telegram_token, telegram_chat_id))
 
         elif "cancel meeting" in query.lower():
-            cancel_meeting(agenda, start_datetime)  
+            cancel_meeting(agenda, start_datetime)
