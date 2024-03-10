824b59ad216b61781b9c58833f3066e3a5675cbb
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2024-03-11 03:32:30 +0530

    implemented os commands

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
+import os
+import webbrowser
 
 def speak(text):
     engine = pyttsx3.init()
@@ -21,8 +23,26 @@ def takecommand():
             return "Some Error Occurred. Sorry from Elysium"
 
 if __name__ == '__main__':
-    print('Welcome to Jarvis AI')
+    print('Welcome to Elysium AI')
     speak("Elysium AI")
     while True:
         print("Listening...")
         query = takecommand()
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
+                 ["google", "https://www.google.com"], ]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                speak(f"Opening {site[0]}")
+                webbrowser.open(site[1])
+
+        if "open music" in query:
+            musicPath = "C:/Users/Ishan/Downloads/downfall.mp3"
+            os.system(f"start {musicPath}")
+
+        elif "the time" in query:
+            hour = datetime.datetime.now().strftime("%H")
+            minutes = datetime.datetime.now().strftime("%M")
+            speak(f"Sir the time is {hour} and {minutes} minutes")
+
+        elif "open camera".lower() in query.lower():
+            os.system("start microsoft.windows.camera:")
